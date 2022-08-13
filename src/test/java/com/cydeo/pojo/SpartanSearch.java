218e9fb43ff59d35db6f43d02a9108edfe3c2a90
shahingadirli <shahingadirli@gmail.com>
2022-08-12 21:06:37 -0400

    day 06

---
+++ src/test/java/com/cydeo/pojo/SpartanSearch.java
@@ -0,0 +1,11 @@
+package com.cydeo.pojo;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class SpartanSearch {
+    private List<Spartan> content;
+    private int totalElement;
+}
