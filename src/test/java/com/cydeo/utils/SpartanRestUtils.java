fa4890949777202ea5875a1ecc6e63fab243734c
shahingadirli <shahingadirli@gmail.com>
2022-08-14 15:58:23 -0400

    day 09

--- src/test/java/com/cydeo/utils/SpartanRestUtils.java
+++ src/test/java/com/cydeo/utils/SpartanRestUtils.java
@@ -1,5 +1,7 @@
 package com.cydeo.utils;
 
+import com.cydeo.pojo.Spartan;
+import com.github.javafaker.Faker;
 import io.restassured.RestAssured;
 
 import static io.restassured.RestAssured.*;
@@ -13,5 +15,30 @@ public class SpartanRestUtils {
                 .then().log().all();
     }
 
+    /**
+     * This method creates object of Spartan pojo class
+     * and assigns random data using Faker class
+     * @return
+     */
+    public static Spartan getNewSpartan() {
+        //create Faker class object to help us generate random values
+        Faker random = new Faker();
+
+        Spartan spartan = new Spartan();
+        spartan.setName(random.name().firstName()); //set random firstname
+        //set random gender. 1 - Female otherWise Male
+        int num = random.number().numberBetween(1,3);
+        if (num == 1) {
+            spartan.setGender("Female");
+        } else {
+            spartan.setGender("Male");
+        }
+        //generate random phone num
+        spartan.setPhone(random.number().numberBetween(1000000000L, 9999999999L));
+
+        //return spartan object to caller
+        return spartan;
+
+    }
 
 }
