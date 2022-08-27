9800a15968b731bd0ac142ef741373e1e9925de0
shahingadirli <shahingadirli@gmail.com>
2022-08-27 12:23:25 -0400

    day 16

--- src/test/java/com/cydeo/tests/day15_data_driven_testing/CsvFileSourceTest.java
+++ src/test/java/com/cydeo/tests/day15_data_driven_testing/CsvFileSourceTest.java
@@ -15,6 +15,14 @@ import static io.restassured.RestAssured.*;
 import static org.hamcrest.MatcherAssert.*;
 import static org.hamcrest.Matchers.*;
 
+import com.cydeo.utils.ConfigurationReader;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static io.restassured.RestAssured.*;
 public class CsvFileSourceTest {
 
     @BeforeAll
---
+++ src/test/java/com/cydeo/tests/day16_methodsource_mocks/MethodSourceTest.java
@@ -0,0 +1,29 @@
+package com.cydeo.tests.day16_methodsource_mocks;
+
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MethodSourceTest {
+
+    /**
+     * data provider method.
+     */
+
+    public static List<String> getCountries(){
+
+        List<String> countries = Arrays.asList("Canada", "USA", "France", "Turkey", "Mexico", "Egypt", "Germany");
+
+        return countries;
+    }
+    @ParameterizedTest
+    @MethodSource("getCountries")
+    public void countriesTest(String countryName){
+        System.out.println("countryName = " + countryName);
+        System.out.println(countryName.length());
+
+    }
+}
